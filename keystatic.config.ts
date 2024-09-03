92395b01e37e2f4dca6a9a6b08515cef3ef1ccbb
Simon Richards <s.richards@dewynters.com>
2024-09-04 00:51:07 +0100

    Update keystatic.config.ts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -3,7 +3,8 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'github',
+    repo:  'sjrichards88/keystatic-astro'
   },
   collections: {
     posts: collection({
